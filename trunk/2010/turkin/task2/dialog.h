9790c0d4da314b34978f89d3c8f56243b6f4c31a
snowwlex <snowwlex@1a14799f-55e9-4979-7f51-533a2053511e>
2010-09-29 14:44:21 +0000

    Task 2

---
+++ trunk/2010/turkin/task2/dialog.h
@@ -0,0 +1,43 @@
+#ifndef _DIALOG_H_
+#define _DIALOG_H_
+
+#include <QWidget>
+#include <QDialog>
+#include <QComboBox>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QTimerEvent>
+
+ 
+class Dialog : public QDialog {
+    Q_OBJECT;
+    
+    public:
+        Dialog(QWidget *parent = NULL);
+        
+    public:
+        virtual void timerEvent(QTimerEvent* e);
+        
+    private: // UI elements
+        QLabel* lblTimer[2];
+        QComboBox* cbChooseTimer;
+        QLineEdit* leInterval;
+        QPushButton* btnSet;
+        QPushButton* btnStop;
+        
+    private:
+        int countTimer[2];
+        int timerId[2];
+        bool isTimerOn[2];
+    
+    signals:
+    
+    public slots:
+        void setTimer();
+        void stopTimer();   
+
+};
+
+
+#endif

9091ede260421cdff0d3ae343ed80ba204f3a69a
M.Baboshina <M.Baboshina@1a14799f-55e9-4979-7f51-533a2053511e>
2010-10-03 13:29:05 +0000

    task2

---
+++ 2010/baboshina/task2/dialog.h
@@ -0,0 +1,40 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QTimerEvent>
+
+class myDialog : public QDialog{
+    Q_OBJECT
+
+public:
+    myDialog(QWidget* p = NULL);
+
+    virtual void timerEvent(QTimerEvent*);
+
+private:
+    int timer_id1, timer_id2;
+    bool isTimerOn1, isTimerOn2;
+    int count1,count2;
+
+    QPushButton* btnSet;
+    QPushButton* btnStop;
+    QLabel* lbl1;
+    QLabel* lbl2;
+    QLabel* lbltimer1;
+    QLabel* lbltimer2;
+    QLineEdit* leInt;
+    QComboBox* cbChoose;
+
+public slots:
+    void set();
+    void stop();
+
+};
+
+#endif

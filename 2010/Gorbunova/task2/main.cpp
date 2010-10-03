fc9bc77e659a72b5956af99fcd01fa38d51b0d23
GorbunovaIN <GorbunovaIN@1a14799f-55e9-4979-7f51-533a2053511e>
2010-10-03 17:36:05 +0000

    task2

---
+++ 2010/Gorbunova/task2/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include "Dialog.h"
+
+int main(int argc, char **argv){
+  QApplication app(argc, argv);
+  Dialog d;
+  d.setWindowTitle("Timers");
+  d.show();
+  return app.exec();
+}
+

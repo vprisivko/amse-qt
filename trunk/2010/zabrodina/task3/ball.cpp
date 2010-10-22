ca2da407d84df85a6c84e70d85615921517161f4
NadyaZabr <NadyaZabr@1a14799f-55e9-4979-7f51-533a2053511e>
2010-10-22 22:23:21 +0000

    new ball checker

--- trunk/2010/zabrodina/task3/ball.cpp
+++ trunk/2010/zabrodina/task3/ball.cpp
@@ -4,79 +4,72 @@ BallDialog ::BallDialog( int _rad, int _vx, int _vy ){
     xradius = _rad;
     yradius = _rad;
     radius = _rad;
-    compressionx = 0;
-    compressiony = 0;
+    setMinimumSize(4*radius,4*radius);
     vx = _vx;
     vy = _vy;
     p = new QPainter();
     bTimer = new QTimer(this);
     connect( bTimer, SIGNAL(timeout()), this, SLOT( timeEvent()) );
-    bTimer->start( 60 );
-    x = 0;
-    y = 0;
+    bTimer->start( 20 );
+    x = width()/2;
+    y = height()/2;
 }
 
-void BallDialog::paintEvent( QPaintEvent * ) {
+void BallDialog::paintEvent( QPaintEvent *) {
+
     p->begin( this );
     p->setBrush( Qt::SolidPattern );
     p->setPen( Qt::green );
-    int x1, y1, x2, y2;
-    this->geometry().getCoords( &x1, &y1, &x2, &y2);
-    p->drawEllipse( QPoint( x + 0.5 * ( x2 - x1 ), y + 0.5 * ( y2 - y1 ) ), xradius, yradius);
+    p->drawEllipse( QPoint( x,y ), xradius, yradius);
     p->end();
+
 }
 
 void BallDialog::timeEvent() {
-    if( compressionx == 0 && compressiony == 0 ) {
-     if( ( x + xradius + vx ) >= this->width()/2  || ( x - xradius + vx ) <= -this->width()/2) {
-        compressionx = 1; //need to press. if compress ==-1 -> need to unpress//
+    /*
+     We need to remember all parameters (width, height, current coordinates of the ball),
+     because our dialog can be resized&moved during this method
+    */
+    int w = width();
+    int h = height();
+    int xx = x;
+    int yy = y;
+     xx += vx;
+     yy += vy;
+
+     int xCurrentRadius = radius;
+     int yCurrebtRadius = radius;
+
+     if ( xx + radius > w){
+       xCurrentRadius = w - xx;
      }
-     else if( ( y + yradius + vy) >= this->height()/2 || ( y - yradius + vy) <= -this->height()/2) {
-        compressiony = 1;
+
+     if ( xx - radius < 0){
+       xCurrentRadius = xx;
      }
-     else{
-     x += vx;
-     y += vy;
-    }
-    }
-    else {
-        int ax = 1 ? compressionx !=0 : 2;
-        press( ax);
-        x += vx;
-        y += vy;
-    }
+
+     if ( yy + radius > h){
+       yCurrebtRadius = h - yy;
+     }
+
+     if ( yy - radius < 0){
+       yCurrebtRadius = yy;
+     }
+     if ( xCurrentRadius < radius/2){
+       vx= -vx;
+     }
+     if ( yCurrebtRadius < radius/2){
+       vy = -vy;
+     }
+
+     x=xx;
+     y=yy;
+     xradius = xCurrentRadius;
+     yradius = yCurrebtRadius;
+
     repaint();
+
 }
 
-void BallDialog :: press( int axes ) {
-    if( axes ==1 ) {
-        if( compressionx == 1 && xradius > radius/2 ) {
-            xradius -= abs(vx);
-        }
-        else if( compressionx == 1 && xradius <= radius/2 ) {
-            compressionx = -1;
-            vx = -vx;
-        }
-        else if( compressionx == -1 && xradius < radius)  {
-            xradius += abs( vx );
-        }
-        else {
-            compressionx = 0;
-        }
-    }
-    else {
-        if( compressiony == 1 && yradius > radius/2 ) {
-            yradius -= abs(vy);
-        }
-        else if( compressiony == 1 && yradius <= radius/2) {
-            compressiony = -1;
-            vy = -vy;
-        }
-        else if( compressiony == -1 && yradius < radius) {
-            yradius += abs(vy);
-        }
-        else {
-            compressiony = 0;
-        }
-    }
-}
+
+

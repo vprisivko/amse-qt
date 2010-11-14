a4470cdfc556eee76189e7f60fc22b55f23648c1
korgovk <korgovk@1a14799f-55e9-4979-7f51-533a2053511e>
2010-11-14 17:34:31 +0000

    task7 added

---
+++ 2010/korgov/task7/test/main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "TestTimerDialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+	
+    TestTimerDialog timerDialog;
+
+    QTest::qExec(&timerDialog);
+
+
+
+    return a.exec();
+}

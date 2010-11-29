c7f108f0ce4908231db9c87dd0fce29187bbfd04
Natalia.Ukhorskaya@gmail.com <Natalia.Ukhorskaya@gmail.com@1a14799f-55e9-4979-7f51-533a2053511e>
2010-11-29 15:02:10 +0000

    task7

--- 2010/zabrodina/task7/test/main.cpp
+++ 2010/zabrodina/task7/test/main.cpp
@@ -1,10 +1,10 @@
-#include<QApplication>
-#include<QTest>
-#include"Test1.h"
+#include <QApplication>
+#include <QTest>
+#include "Test.h"
 
-int main(int c, char* v[]){
-    QApplication app(c,v);
-    Test1 test;
+int main(int argc, char *argv[]){
+    QApplication app(argc, argv);
+    Test test;
     QTest::qExec(&test);
     app.exec();
 }
